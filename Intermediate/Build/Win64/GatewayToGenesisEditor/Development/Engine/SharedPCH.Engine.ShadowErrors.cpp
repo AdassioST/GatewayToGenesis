4fe80a072fbebcd6e7409160523c8218b459c0c6
Adassio ST <42128563+AdassioST@users.noreply.github.com>
2021-01-03 07:01:43 -0600

    Segunda subida inicial
    Segunda subida de archivos

---
+++ Intermediate/Build/Win64/GatewayToGenesisEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.26.28806
+#include "C:/Users/Adass/Documents/Unreal Projects/GatewayToGenesis/Intermediate/Build/Win64/GatewayToGenesisEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/GatewayToGenesis/GatewayToGenesisCharacter.generated.h
@@ -0,0 +1,97 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef GATEWAYTOGENESIS_GatewayToGenesisCharacter_generated_h
+#error "GatewayToGenesisCharacter.generated.h already included, missing '#pragma once' in GatewayToGenesisCharacter.h"
+#endif
+#define GATEWAYTOGENESIS_GatewayToGenesisCharacter_generated_h
+
+#define GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_SPARSE_DATA
+#define GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_RPC_WRAPPERS
+#define GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAGatewayToGenesisCharacter(); \
+	friend struct Z_Construct_UClass_AGatewayToGenesisCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AGatewayToGenesisCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/GatewayToGenesis"), NO_API) \
+	DECLARE_SERIALIZER(AGatewayToGenesisCharacter)
+
+
+#define GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAGatewayToGenesisCharacter(); \
+	friend struct Z_Construct_UClass_AGatewayToGenesisCharacter_Statics; \
+public: \
+	DECLARE_CLASS(AGatewayToGenesisCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/GatewayToGenesis"), NO_API) \
+	DECLARE_SERIALIZER(AGatewayToGenesisCharacter)
+
+
+#define GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AGatewayToGenesisCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AGatewayToGenesisCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AGatewayToGenesisCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AGatewayToGenesisCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AGatewayToGenesisCharacter(AGatewayToGenesisCharacter&&); \
+	NO_API AGatewayToGenesisCharacter(const AGatewayToGenesisCharacter&); \
+public:
+
+
+#define GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AGatewayToGenesisCharacter(AGatewayToGenesisCharacter&&); \
+	NO_API AGatewayToGenesisCharacter(const AGatewayToGenesisCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AGatewayToGenesisCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AGatewayToGenesisCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AGatewayToGenesisCharacter)
+
+
+#define GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__CameraBoom() { return STRUCT_OFFSET(AGatewayToGenesisCharacter, CameraBoom); } \
+	FORCEINLINE static uint32 __PPO__FollowCamera() { return STRUCT_OFFSET(AGatewayToGenesisCharacter, FollowCamera); }
+
+
+#define GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_9_PROLOG
+#define GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_SPARSE_DATA \
+	GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_RPC_WRAPPERS \
+	GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_INCLASS \
+	GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_SPARSE_DATA \
+	GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> GATEWAYTOGENESIS_API UClass* StaticClass<class AGatewayToGenesisCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID GatewayToGenesis_Source_GatewayToGenesis_GatewayToGenesisCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
